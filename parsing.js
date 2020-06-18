494f1a99608af664270374f334a83e8de1b4d531
unknown <hmuley22@gmail.com>
2020-06-18 16:51:45 +0530

    Added routing file and use of import system from routing to parsing.js file

--- parsing.js
+++ parsing.js
@@ -1,33 +1,4 @@
 const http = require('http');
-const fs = require('fs')
-http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method;
-    if (url == '/') {
-        res.setHeader('Content-Type', 'text/html')
-        res.write('<title> Home </title>');
-        res.write('<h1> Enter details </h1>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message" placeholder="enter value"><button type="submit">Submit</button>')
-        return res.end();
-    }
-    if (url == '/message' && method == "POST"){
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
-        });
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
-    }
-    res.setHeader('Content-Type', 'text/html')
-    res.write('<title> About </title>');
-    res.write('<h1> This is my first node response </h1>');
-    res.end();
-}).listen(8080); 
+const routing = require('./routing');
+const fs = require('fs');
+http.createServer(routing).listen(8080); 
